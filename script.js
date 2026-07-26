d7b5d715446c0e8a1c0d0d86d8ac0691b80cb7e1
antoineyammine-code <antoineyammine@gmail.com>
2026-07-26 04:50:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 { name: "Grilled Chicken Burger", price: "275.000 LL", description: "grilled chicken patty, lettuce, cheddar cheese and chicken BBQ special sauce." },
               /*{ name: "Crispy Chicken Burger", price: "325.000 LL", description: "chicken crispy, lettuce, cheddar cheese and chicken bbq special sauce." },*/
                 { name: "Soujok Burger", price: "325.000 LL", description: "soujok mild hot patty, lettuce, cheddar cheese, pickles, tomatoes and soujok special sauce." },
-                { name: "Fahita", price: "500.000 LL", description: "chicken, mozarella, bell pepper, onion, mushrooms and avocado sauce." },
+                { name: "Big Fahita", price: "500.000 LL", description: "400grs, chicken, mozarella, bell pepper, onion, mushrooms and avocado sauce." },
                 { name: "Fish Burger", price: "275.000 LL", description: "white fish fillet, lettuce, cheddar cheese and fish special sauce." },
                 { name: "Double Cheese Burger", price: "450.000 LL", description: "double meat patties, double cheese, lettuce, special sauce." },
                 { name: "Double Grilled Chicken Burger", price: "450.000 LL", description: "double grilled chicken patty, double cheese, lettuce and chicken BBQ special sauce." },
